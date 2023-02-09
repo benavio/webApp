044d73ee1c42daf1db0106d0786d52f183f0d75f
benavio <dt76tank@gmail.com>
2023-02-09 10:14:03 +0300

    "/people" delete "selections"

--- src/main/java/ru/benavio/webApp/biz/model/Person.java
+++ src/main/java/ru/benavio/webApp/biz/model/Person.java
@@ -3,6 +3,7 @@ package ru.benavio.webApp.biz.model;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
+import jakarta.validation.constraints.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -19,10 +20,22 @@ public class Person {
     @Id
     @GeneratedValue
     private Long id;
+
+    @NotEmpty(message= "Please enter: First name")
     private String firstname;
+
+    @NotEmpty(message= "Please enter: Last name")
     private String lastname;
+
+    @Past(message= "Date of birth must be in the past")
+    @NotNull(message = "Date of birth must be valid")
     private LocalDate dob;
+
+    @NotEmpty(message = "Email must be empty")
+    @Email(message = "Email must be valid")
     private String email;
+    @DecimalMin(value = "1000.00",message = "Salary must be least then 1000")
+    @NotNull(message = "Salary can not be empty")
     private BigDecimal salary;
 
 
--- src/main/java/ru/benavio/webApp/web/controller/PeopleController.java
+++ src/main/java/ru/benavio/webApp/web/controller/PeopleController.java
@@ -1,17 +1,19 @@
 package ru.benavio.webApp.web.controller;
 
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotNull;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.validation.Errors;
+import org.springframework.web.bind.annotation.*;
 import ru.benavio.webApp.biz.model.Person;
 import ru.benavio.webApp.data.PersonRepository;
 
 import java.math.BigDecimal;
 import java.time.LocalDate;
 import java.util.List;
+import java.util.Optional;
 
 @Controller
 @RequestMapping("/people")
@@ -31,13 +33,28 @@ public class PeopleController {
         return new Person();
     };
 
+
     @GetMapping
     public String showPeoplePage(){
         return "people";
     };
+
     @PostMapping
-    public String savePerson(Person person){
-        personRepository.save(person);
+    public String savePerson(@Valid Person person, Errors errors){
+        if(!errors.hasErrors())
+        {
+            personRepository.save(person);
+            return "redirect:people";
+        }
+        return "people";
+    }
+
+    @PostMapping(params = "delete=true")
+    public String deletePeople(@RequestParam Optional<List<Long>> selections){
+        System.out.println(selections);
+        if(selections.isPresent()) {
+            personRepository.deleteAllById(selections.get());
+        }
         return "redirect:people";
     }
 
